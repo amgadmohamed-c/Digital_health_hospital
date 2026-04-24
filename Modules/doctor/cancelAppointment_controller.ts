b2a27b8b7bbeaf01da375cec10cb8e0055957e50
amgad-shaaban <magoodmessi369@gmail.com>
2026-04-24 17:55:39 +0300

    feat(doctor):doctor cancel appointment

---
+++ Modules/doctor/cancelAppointment_controller.ts
@@ -0,0 +1,33 @@
+import { Request , Response } from "express";
+import cancelAppointment from "./cancelAppointment_service";
+import { isStaff } from "../emergency/createCase_service";
+import { JwtPayload } from "jsonwebtoken";
+
+
+export default async function doctorCancelAppointment(req:Request , res : Response) {
+    const {email} = req.user as JwtPayload ; 
+    if(!email){
+        return res.status(401).json({err:"email doesnt exist"});
+    }
+    try{
+        const staff = await isStaff(email);
+        console.log(staff);
+        const data = {
+            appointmentId: req.body.id , 
+            appointmentStatus : req.body.appintmentStatus
+        }
+        const cancelledAppointment = await cancelAppointment(data);
+        if(!cancelAppointment){
+            throw new Error("couldnt cancel")
+
+        }
+        return res.status(200).json({message : "deleted appointment successfully"});
+    }
+    catch(err:any){
+        console.log(err.message);
+        return res.status(400).json({
+            err:err.message
+        }) ; 
+    }
+    
+}
--- Modules/doctor/doctorRouter.ts
+++ Modules/doctor/doctorRouter.ts
@@ -1,7 +1,9 @@
 import express, { Router  } from "express"
 import doctorSlots from "./availability_controller";
 import appointmentBook from "./bookAppointment_controller";
+import doctorCancelAppointment from "./cancelAppointment_controller";
 
 const router:  Router = express.Router();
 router.get("/available/appointment/slots" , doctorSlots);
 router.post("/new/appointment" , appointmentBook);
+router.patch("/cancel/appointments" , doctorCancelAppointment);
